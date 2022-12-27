4a3e55acc8837f40e20cedbc2dc3e0cd84cd08c1
Hector Espert <h.espert@sesametime.com>
2022-12-27 11:20:04 +0100

    Fix connection

--- tests/_resources/init.php
+++ tests/_resources/init.php
@@ -2,10 +2,10 @@
 
 // determine the test environment first
 if (getenv('TRAVIS')) {
-    $pdo = new PDO("mysql:dbname=fluentdb;host=localhost;charset=utf8", "root");
+    $pdo = new PDO("mysql:dbname=fluentdb;host=127.0.0.1;charset=utf8", "root");
 }
 else {
-    $pdo = new PDO("mysql:dbname=fluentdb;host=localhost;charset=utf8", "vagrant", "vagrant");
+    $pdo = new PDO("mysql:dbname=fluentdb;host=127.0.0.1;charset=utf8", "vagrant", "vagrant");
 }
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
